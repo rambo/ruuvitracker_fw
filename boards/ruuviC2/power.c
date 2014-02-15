19a3f744592f0928c7a5cd0e3c4e183d570c86ed
Eero af Heurlin <rambo@iki.fi>
2014-02-15 20:19:43 +0200

    untested helpers for entering STOP and STANDBY modes

--- boards/ruuviC2/power.c
+++ boards/ruuviC2/power.c
@@ -95,3 +95,59 @@ static void disable_gsm_fet(void)
 {
      palClearPad(GPIOC, GPIOC_ENABLE_GSM_VBAT);
 }
+
+void power_enter_stop(void)
+{
+    register_power_wakeup_callback(power_wakeup_callback);
+
+    SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
+    PWR->CR |= (PWR_CR_LPDS | PWR_CR_CSBF | PWR_CR_CWUF);
+    PWR->CR &= ~PWR_CR_PDDS;
+    __WFI();
+}
+
+void power_enter_standby(void)
+{
+    // Actually when coming back from standby we don't have this anymore since it's equivalent to a reset...
+    register_power_wakeup_callback(power_wakeup_callback);
+
+    chSysLock();
+    SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
+    PWR->CR |= (PWR_CR_PDDS | PWR_CR_LPDS | PWR_CR_CSBF | PWR_CR_CWUF);
+    RTC->ISR &= ~(RTC_ISR_ALRBF | RTC_ISR_ALRAF | RTC_ISR_WUTF | RTC_ISR_TAMP1F | RTC_ISR_TSOVF | RTC_ISR_TSF);
+    __WFI();
+}
+
+void register_power_wakeup_callback(extcallback_t cb)
+{
+    EXTChannelConfig button_cfg;
+    EXTChannelConfig rtc_cfg;
+
+    button_cfg.cb = cb;
+    button_cfg.mode = EXT_CH_MODE_RISING_EDGE | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOA;
+    // PA0
+    extSetChannelMode(&EXTD1, 0, &button_cfg);
+    
+    rtc_cfg.cb = cb;
+    rtc_cfg.mode = EXT_CH_MODE_RISING_EDGE | EXT_CH_MODE_AUTOSTART;
+    // RTC_ALARM
+    extSetChannelMode(&EXTD1, 17, &rtc_cfg);
+    // RTC_WKUP
+    extSetChannelMode(&EXTD1, 22, &rtc_cfg);
+
+    // I'm not sure if this is required
+    extChannelEnable(&EXTD1, 0);
+    extChannelEnable(&EXTD1, 17);
+    extChannelEnable(&EXTD1, 22);
+}
+
+void power_wakeup_callback(EXTDriver *extp, expchannel_t channel)
+{
+    // TODO: Check if we actually were in STOP mode and only reinit clock in that case
+    chSysLockFromIsr();
+    stm32_clock_init();
+    chSysUnlockFromIsr();
+    // TODO: Raise an event (so other interested parties can know about the event that woke us up), for now just suppress the warnings
+    (void)extp;
+    (void)channel;
+}
